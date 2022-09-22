e52c7c366856e0568be342988e8b60ed340951df
Raphael Seguin <raphaelseguin@gmail.com>
2022-09-22 22:30:18 +0200

    add min max looping drift

--- README.md
+++ README.md
@@ -22,3 +22,13 @@ OPTIONS:
 - -i 1 -s 10000 -r 5 -p 0.2
 - -i 100000 -s 1 -r 20 -p 0.25
 - -i 1 -s 1 -r 1 -p 0.25
+cargo run -- -d 100000 -i 1000000 -r 100 -s 1 -p 10 -a
+cargo run -- -d 100000 -i 1000 -r 1000 -s 1 -p 1000 -a
+cargo run -- -d 100000 -r 5 -s 1 -p 2 -a -i 200000 -n 16 --looping --drift 4
+cargo run -- -d 100000 -r 100 -s 1 -p 4 -i 2 -n 8 --looping --drift 2 -a
+cargo run -- -d 100000 -r 20 -s 1 -p 20 -i 9 -n 10 --looping --drift 1 -a
+cargo run -- -d 100000 -r 20 -s 1 -p 120 -i 9 -n 100 --looping --drift 2
+cargo run -- -d 100000 -r 4 -s 1 -p 0.5 -i 10000 -n 16 --looping --drift 1 -a
+cargo run -- -d 100000 -r 4 -s 1 -p 8 -i 10000 -n 16 --looping --drift 1
+cargo run -- -d 100000 -r 10 -s 3 -p 0.7777 -i 10000 -n 16 --looping --drift 8 -a
+cargo run -- -d 100000 -r 1500 -s 2 -p 50 -i 1000 -n 20 --looping --drift 1 -a
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::time::Duration;
 use std::fs::File;
 use std::path::Path;
 use std::io::Error;
-use rodio::{OutputStream, Source};
+use rodio::{OutputStream};
 use clap::{Parser};
 use wav;
 
@@ -17,6 +17,7 @@ use collatz::Collatz;
 // params in audio file output name
 // atodb dbtoa
 // ramp si plusieurs arguments
+// loop -> valeur -end
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
@@ -25,6 +26,10 @@ struct Args {
    duration: u64,
    #[clap(short, long, value_parser, default_value_t = 1)]
    init: u128,
+   #[clap(short, long, value_parser, default_value_t = 1000)]
+   nsteps: u128,
+   #[clap(long, value_parser, default_value_t = 0)]
+   drift: u128,
    #[clap(short, long, value_parser, default_value_t = 1)]
    step: u128,
    #[clap(short, long, value_parser, default_value_t = 1)]
@@ -35,13 +40,15 @@ struct Args {
    pitch: f32,
    #[clap(short, long, value_parser, default_value_t = false)]
    altern_phase: bool,
-   #[clap(short, long, value_parser)]
-   list: Vec<f32>
+   #[clap(short, long, value_parser, default_value_t = false)]
+   looping: bool,
+//    #[clap(short, long, value_parser)]
+//    list: Vec<f32>
 }
 
 fn main() -> Result<(), Error> {
     let args = Args::parse();
-    println!("{} {} {}", args.list[0], args.list[1], args.list[2]);
+
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
     
     let mut collatz = Collatz::new(args.init);
@@ -49,6 +56,10 @@ fn main() -> Result<(), Error> {
     collatz.repeats = args.repeats;
     collatz.altern_phase = args.altern_phase;
     collatz.set_pitch(args.pitch);
+    collatz.max_value = args.init + args.nsteps;
+    collatz.looping = args.looping;
+    collatz.drift = args.drift;
+
     if args.output.len() > 0 {
         let mut audio: Vec<f32> = Vec::new();
         for _n in 0..(args.duration as f32* 44.1) as usize {
